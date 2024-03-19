35aa8b1f05a55fb5fc6cbe4d68117360705bc91b
LiadOvdat5 <LiadOvdat5@gmail.com>
2024-03-19 19:43:06 +0200

    Add react-router-dom and create route components

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import CreateEvent from "./pages/CreateEvent";
+import EventDetails from "./pages/EventDetails";
+import GuestList from "./pages/GuestList";
+import TaskList from "./pages/TaskList";
+import Vendors from "./pages/Vendors";
+
 function App() {
-  return <div>Work</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/createEvent" element={<CreateEvent />} />
+        <Route path="/eventDetails" element={<EventDetails />} />
+        <Route path="/guestList" element={<GuestList />} />
+        <Route path="/taskList" element={<TaskList />} />
+        <Route path="/vendors" element={<Vendors />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
