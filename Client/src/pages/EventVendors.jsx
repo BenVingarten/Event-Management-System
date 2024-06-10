eb6f7b948974b7d390106554e951c616397988d5
LiadOvdat5 <LiadOvdat5@gmail.com>
2024-06-10 10:56:50 +0300

    Add Vendor Manually to event vendors

--- Client/src/pages/EventVendors.jsx
+++ Client/src/pages/EventVendors.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
-import { Button, Card } from "flowbite-react";
+import { Button, Card, Modal, TextInput } from "flowbite-react";
 import { CiCircleRemove } from "react-icons/ci";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { useLocation } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import useAuth from "../hooks/useAuth";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { Toaster, toast } from "react-hot-toast";
 
 function EventVendors() {
   const axiosPrivate = useAxiosPrivate();
@@ -13,10 +14,16 @@ function EventVendors() {
   const location = useLocation();
   const eventID = location.state.eventId;
   const userId = jwtDecode(auth.accessToken).userInfo.id;
+  const [modalOpen, setModalOpen] = useState(false);
 
   const [eventVendors, setEventVendors] = useState([]);
   const [negotiatiatedVendors, setNegotiatedVendors] = useState([]);
   const [suggestedVendors, setSuggestedVendors] = useState([]);
+  const [newVendor, setNewVendor] = useState({
+    username: "",
+    email: "",
+    businessType: "",
+  });
 
   useEffect(() => {
     // fetch event vendors
@@ -29,6 +36,7 @@ function EventVendors() {
 
   const StartNegotiationWithVendor = (vendor) => {
     console.log("Start Negotiation with Vendor");
+    toast.success("Email Sent to Vendor!");
     console.log(vendor);
     // Add to Negotiated Vendors
     setNegotiatedVendors((pv) => [...pv, vendor]);
@@ -41,6 +49,7 @@ function EventVendors() {
 
   const AddVendorToMyVendors = (vendor) => {
     console.log("Add Vendor to My Vendors");
+    toast.success("Vendor Added to Event Vendors!");
     console.log(vendor);
     // Add to Event Vendors
     setEventVendors((pv) => [...pv, vendor]);
@@ -53,6 +62,7 @@ function EventVendors() {
 
   const RemoveVendorFromNegotiatedVendors = (vendor) => {
     console.log("Remove Vendor from Negotiated Vendors");
+    toast.success("Vendor Removed from Negotiated Vendors!");
     console.log(vendor);
     // Remove from Negotiated Vendors
     setNegotiatedVendors((pv) =>
@@ -62,13 +72,29 @@ function EventVendors() {
 
   const RemoveVendorFromEventVendors = (vendor) => {
     console.log("Remove Vendor from Event Vendors");
+    toast.success("Vendor Removed from Event Vendors!");
     console.log(vendor);
     // Remove from Event Vendors
     setEventVendors((pv) => pv.filter((v) => v.username !== vendor.username));
   };
 
+  const AddVendorManually = () => {
+    console.log("Add Vendor Manually");
+    toast.success("Vendor Added Manually!");
+    console.log(newVendor);
+    // Add to Event Vendors
+    setEventVendors((pv) => [...pv, newVendor]);
+    // Close Modal
+    setModalOpen(false);
+  };
+
+  const handleInputChange = (e) => {
+    setNewVendor({ ...newVendor, [e.target.id]: e.target.value });
+  };
+
   return (
     <div className="grid sm:grid-rows-3 md:grid-cols-3 pl-5">
+      <Toaster />
       {/* Event Vendors */}
       <div className="overflow-y-auto">
         <h1 className="text-2xl font-bold mb-2">Event Vendors</h1>
@@ -91,6 +117,55 @@ function EventVendors() {
             </div>
           </Card>
         ))}
+        <Button color={"green"} onClick={() => setModalOpen(true)}>
+          Add Vendor Manually
+        </Button>
+        <Modal show={modalOpen} onClose={() => setModalOpen(false)}>
+          {/* Add Vendor Manually */}
+
+          <form className="p-5 " onSubmit={AddVendorManually}>
+            <h1 className="text-2xl font-bold mb-2">Add Vendor Manually</h1>
+            <TextInput
+              id="username"
+              type="text"
+              className="p-2"
+              placeholder="Vendor Name"
+              onChange={handleInputChange}
+              required
+            />
+            <TextInput
+              id="email"
+              type="email"
+              placeholder="Vendor Email"
+              className="p-2"
+              required
+              onChange={handleInputChange}
+            />
+            <TextInput
+              id="businessType"
+              type="text"
+              className="p-2"
+              placeholder="Vendor Business Type"
+              onChange={handleInputChange}
+              required
+            />
+            <Button
+              color={"green"}
+              className="m-2"
+              //onClick={AddVendorManually}
+              type="submit"
+            >
+              Add Vendor
+            </Button>
+            <Button
+              color={"red"}
+              className="m-2"
+              onClick={() => setModalOpen(false)}
+            >
+              Cancel
+            </Button>
+          </form>
+        </Modal>
       </div>
 
       {/* Negotiated Vendors */}
